6a55861018abd119d196d661bbdc8cb2b979d5bb
Zarwine <ange_550@hotmail.com>
2021-05-21 10:20:02 +0200

    Mise en place d'un systeme de login/logout + Formulaire

--- FriendsOfSymfony/FOSProject/src/Security/LoginFormAuthenticator.php
+++ FriendsOfSymfony/FOSProject/src/Security/LoginFormAuthenticator.php
@@ -96,7 +96,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        return new RedirectResponse($this->urlGenerator->generate('app_homepage'));
+        return new RedirectResponse($this->urlGenerator->generate('app_home'));
     }
 
     protected function getLoginUrl()
